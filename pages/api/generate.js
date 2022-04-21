09b2a1110a54c31d34b2b09c9bae312c363ef517
Jakob Vicari <kontakt@jakobvicari.de>
2022-04-21 09:01:12 +0200

    Add files via upload
    German version

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -8,7 +8,7 @@ const openai = new OpenAIApi(configuration);
 export default async function (req, res) {
   const completion = await openai.createCompletion("text-davinci-002", {
     prompt: generatePrompt(req.body.animal),
-    temperature: 0.6,
+    temperature: 0.4,
   });
   res.status(200).json({ result: completion.data.choices[0].text });
 }
@@ -18,10 +18,14 @@ function generatePrompt(animal) {
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
   return `Suggest three names for an animal that is a superhero.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
+Animal: Katze
+Names: Kapitän Scharfklaue, Herr Flauschball, Fellissimo
+Animal: Hund
+Names: Super Rex, Wunderhund, König Bello
+Animal: Wellensittich
+Names: Blaubart, Flyderman, Wellinator 
+Animal: Hase
+Names: Superklopfer, Pfotinator, Flauschator
 Animal: ${capitalizedAnimal}
 Names:`;
 }
