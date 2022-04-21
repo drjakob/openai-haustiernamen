155ecebcc31a45da5703eea3ccc25c168d455d53
Jakob Vicari <kontakt@jakobvicari.de>
2022-04-21 08:53:53 +0200

    Add files via upload
    Changed index to German

--- pages/index.js
+++ pages/index.js
@@ -19,29 +19,39 @@ export default function Home() {
     setResult(data.result);
     setAnimalInput("");
   }
+  
+
 
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>Haustiernamen-Finder</title>
+        <link rel="icon" href="/pettag.svg" />
       </Head>
 
       <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
+        <img src="/supercat.svg" className={styles.icon} />
+        <h3>Benenn dein Super-Haustier</h3>
+                <p>Keine Katze muss Muschi heißen. Unsere Haustiere sind Superhelden! Drück auf den Button und finde für deinen neuen Mitbewohner einen Superheldennamen. Inspiriert von den bewunderten Stars aus deinen Lieblingsfilmen und unserer einzigartigen Künstlichen Intelligenz. Hast du eine gelbe Katze, einen schwarzen Hund oder ein karriertes Meerschweinchen?</p>
+
         <form onSubmit={onSubmit}>
           <input
             type="text"
             name="animal"
-            placeholder="Enter an animal"
+            placeholder="Gib ein Tier ein"
             value={animalInput}
             onChange={(e) => setAnimalInput(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
+          <input type="submit" value="Finde einen Namen" />
         </form>
+
         <div className={styles.result}>{result}</div>
+                    <a target="_blank" href="https://www.amazon.de/b?_encoding=UTF8&tag=wissensjakobv-21&linkCode=ur2&linkId=cfc1b40bcfc854da7647c2bc20db8c2c&camp=1638&creative=6742&node=340852031">Hast du Futternapf, Spielzeug, Bett? Die besten Dinge für deinen Liebling gibts auf amazon. (Affiliate-Link)</a>
+
       </main>
     </div>
+    
   );
+
 }
+
